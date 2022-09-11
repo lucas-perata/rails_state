bdbcd1bb6c1a34bca51252a6fc8fdd137a1682b0
lucas-perata <contacto.perata@gmail.com>
2022-09-11 16:07:45 -0300

    Agregado de Dashboard: controller, routes, views, test, models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get "/dashboard" => 'dashboard#index', as: :dashboard
+  get 'dashboard/properties'
   resources :properties
   devise_for :accounts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
