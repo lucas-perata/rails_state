f170b46b89999c715839801e9cae6fc267a0c847
lucas-perata <contacto.perata@gmail.com>
2022-09-14 11:24:09 -0300

    Agent detalles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+    def profile_picture account
+        thumb = account.avatar.attached? ? (url_for account.avatar.variant(resize_to_limit: [150, 150])) : "placeholder.jpg"
+    end 
 end
+    
