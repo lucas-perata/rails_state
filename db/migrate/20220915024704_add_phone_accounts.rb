943f3c4af777b82ef16f05291f37198cc0f44e1f
lucas-perata <contacto.perata@gmail.com>
2022-09-14 23:49:47 -0300

    Agregado de phone para agentes

---
+++ db/migrate/20220915024704_add_phone_accounts.rb
@@ -0,0 +1,5 @@
+class AddPhoneAccounts < ActiveRecord::Migration[7.0]
+  def change
+    add_column :accounts, :phone, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_011355) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_024704) do
   create_table "accounts", force: :cascade do |t|
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_011355) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "company"
+    t.string "phone"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
