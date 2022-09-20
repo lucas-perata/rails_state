c98fd81889482cce24f3debf812e0ed4f8be1d2c
lucas-perata <contacto.perata@gmail.com>
2022-09-19 22:46:33 -0300

    Migracion admin to accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_024704) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_014546) do
   create_table "accounts", force: :cascade do |t|
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_024704) do
     t.datetime "updated_at", null: false
     t.string "company"
     t.string "phone"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
