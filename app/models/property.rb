fd96ffad70ab0097f6ec276dde3b86bd9ed1eea8
lucas-perata <contacto.perata@gmail.com>
2022-09-28 22:53:45 -0300

    Agregado de scopes sold, rented, sale, rent modelo de propiedad

--- app/models/property.rb
+++ app/models/property.rb
@@ -5,4 +5,8 @@ class Property < ApplicationRecord
   has_many_attached :images
 
   scope :latest, -> { order created_at: :desc }
+  scope :sold, -> {where status: "Sold"}
+  scope :rented, -> {where status: "Rented"}
+  scope :sale, -> {where status: "Sell"}
+  scope :rent, -> {where status: "Rent"}
 end
