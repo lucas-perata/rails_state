5bf0603ef0d091e5df3632897202f74716c7e4c0
lucas-perata <contacto.perata@gmail.com>
2022-10-26 23:12:58 -0300

    Agregado de imagenes para posts con AS y estilos creacion

---
+++ db/migrate/20221027021001_eliminate_column_posts.rb
@@ -0,0 +1,5 @@
+class EliminateColumnPosts < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :posts, :image
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_014552) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_021001) do
   create_table "accounts", force: :cascade do |t|
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
@@ -72,7 +72,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_014552) do
     t.string "url"
     t.string "summary"
     t.text "body"
-    t.string "image"
     t.integer "account_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
